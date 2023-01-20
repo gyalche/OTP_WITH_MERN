9302ff43e4f81d4589449e784ccf230f2de65888
Dawa <sherpadawa01@outlook.com>
2023-01-20 21:20:13 +0545

    password page done

--- client/src/components/Password.jsx
+++ client/src/components/Password.jsx
@@ -1,7 +1,60 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from '../styles/Username.module.css';
+import { Toaster } from 'react-hot-toast';
+import { useFormik } from 'formik';
+import avatar from '../assets/profile.png';
+import { validatePassword } from '../helper/validate';
 
 const Password = () => {
-  return <div>Password</div>;
+  const formik = useFormik({
+    initialValues: { password: '' },
+    validateOnBlue: false,
+    validateOnChange: false,
+    validate: validatePassword,
+    onSubmit: async (values) => {
+      console.log(values);
+    },
+  });
+  return (
+    <div className="container mx-auto">
+      <Toaster position="top-center" reverseOrder={false}></Toaster>
+      <div className="flex justify-center items-center h-full">
+        <div className={styles.glass}>
+          <div className="title flex flex-col items-center">
+            <h4 className="text-5xl font-bold">Hello</h4>
+            <span className="py-4 text-xl w-2/3 text-center text-gray-500">
+              Explore More by Connecting with us
+            </span>
+          </div>
+          <form className="py-1" onSubmit={formik.handleSubmit}>
+            <div className="profile flex justify-center py-4">
+              <img src={avatar} className={styles.profile_img} alt="avatar" />
+            </div>
+            <div className="textbox flex flex-col items-center gap-6">
+              <input
+                className={styles.textbox}
+                type="password"
+                placeholder="password"
+                {...formik.getFieldProps('password')}
+              />
+              <button type="submit" className={styles.btn}>
+                Sign In
+              </button>
+            </div>
+            <div className="text-center py-4">
+              <span className="text-gray-500">
+                Forgot password{' '}
+                <Link className="text-red-500" to="/recovery">
+                  Recover password
+                </Link>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Password;
--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -1,10 +1,27 @@
 import toast from 'react-hot-toast';
 
 //validate login page username;
-
 export async function usernameValidate(values) {
   const errors = usernameVerify({}, values);
   return errors;
+}
+export async function validatePassword(values) {
+  const errors = passwordVerify({}, values);
+  return errors;
+}
+//validate password;
+function passwordVerify(error = {}, values) {
+  const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+  if (!values.password) {
+    error.password = toast.error('Password required');
+  } else if (values.password.includes(' ')) {
+    error.password = toast.error('Wrong Password');
+  } else if (values.password.length < 4) {
+    error.password = toast.error('Password must be more than 4 characters');
+  } else if (!specialChars.test(values.password)) {
+    error.password = toast.error('Password must have specail character');
+  }
+  return error;
 }
 //validate username;
 function usernameVerify(error = {}, values) {
